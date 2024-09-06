0aa650905e90ff22113116cfdb34c56cfab0f1e1
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-06 21:51:20 +0300

    completed compiling code as per instructions

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        #initializes with empty list attribute "knowledge"
+        self.knowledge = []
+
+    def learn(self, new_info_string):
+        self.knowledge.append(new_info_string)
+        
